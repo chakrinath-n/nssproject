8e8a6d5a1c506a136f84af3f203bb7e3654ddd7d
unknown <laghumarapuchakrinath.com>
2026-04-28 12:14:04 +0530

    Reports URL

--- frontend/src/admin/pages/Reports.tsx
+++ frontend/src/admin/pages/Reports.tsx
@@ -9,7 +9,7 @@ import {
   type Report,
 } from "../services/report.api";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = `${import.meta.env.VITE_API_URL}`;
 
 export default function Reports() {
   const [reports, setReports] = useState<Report[]>([]);
