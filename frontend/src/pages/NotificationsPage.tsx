a2405ae293891b60445ed6d054899536500ef1b0
unknown <laghumarapuchakrinath.com>
2026-04-28 12:27:57 +0530

    Notification URL

--- frontend/src/pages/NotificationsPage.tsx
+++ frontend/src/pages/NotificationsPage.tsx
@@ -8,7 +8,7 @@ import {
 } from "lucide-react";
 import api from "@/api/axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = `${import.meta.env.VITE_API_URL}`;
 
 interface Notification {
   id: number;
