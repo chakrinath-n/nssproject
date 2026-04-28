cceda1edabe9e3dcca9126598fbdd3ab677c5f89
unknown <laghumarapuchakrinath.com>
2026-04-28 12:05:53 +0530

    Add URL in Reports

--- frontend/src/pages/ReportsPage.tsx
+++ frontend/src/pages/ReportsPage.tsx
@@ -9,7 +9,7 @@ import {
 } from "lucide-react";
 import api from "@/api/axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = `${import.meta.env.VITE_API_URL}`;
 
 interface Report {
   id: number;
